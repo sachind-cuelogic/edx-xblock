0c94fb0bf0808d1acb079d78662e42588a10a325
Sachin Dane <sachin.dane@cuelogic.com>
2017-09-08 12:23:22 +0530

    update ref view

---
+++ referencexblock/referencexblock.py
@@ -0,0 +1,123 @@
+"""TO-DO: Write a description of what this XBlock is."""
+
+import os
+import pkg_resources
+
+from xblock.core import XBlock
+from xblock.fields import Scope, Integer, String, Dict
+from xblock.fragment import Fragment
+from mako.template import Template
+from .utils import loader, AttrDict
+from xblockutils.studio_editable import StudioEditableXBlockMixin
+
+class ReferenceXBlock(XBlock):
+    """
+    TO-DO: document what your XBlock does.
+    """
+
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
+
+    ref_name = String(display_name = "Reference Name", default=None, scope=Scope.content)
+    ref_link = String(display_name = "Reference Link", help="Reference link", default=None, scope=Scope.content)
+    ref_description = String(display_name = "Reference Description", help="Reference Description", default=None, scope=Scope.content)
+    ref_type = String(display_name = "Reference Type", default=None, scope=Scope.content)
+    ref_status = String(display_name = "Reference Status", help="Reference Name", default=None, scope=Scope.content)
+
+
+    def resource_string(self, path):
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
+
+
+    # TO-DO: change this view to display your data your own way.
+    def student_view(self, context=None):
+        """
+        The primary view of the ReferenceXBlock, shown to students
+        when viewing courses.
+        """
+
+        context = {
+        'ref_name': self.ref_name,
+        'ref_link': self.ref_link,
+        'ref_type': self.ref_type,
+        'ref_description': self.ref_description,
+        'ref_status': self.ref_status
+        }
+        fragment = Fragment()
+        fragment.add_content(loader.render_template('static/html/referenceview.html', context))
+        fragment.add_css(self.resource_string("static/css/referencexblock.css"))
+        return fragment   
+
+
+        # fragment = Fragment(loader.render_template("static/html/referencexblock.html", context))
+        # fragment.add_css(self.resource_string("static/css/referencexblock.css"))
+        # fragment.add_javascript(
+        # self.resource_string("static/js/src/referencexblock.js"))
+        # fragment.initialize_js("ReferenceXBlock")
+        # return fragment
+
+
+    """
+    In studio view staff can able to edit reference form
+    """
+    def studio_view(self, context=None):
+        """
+        This is the view that reference content will see when they click on the
+        "Edit" button in Studio.
+        """
+
+        context = {
+        'ref_name': self.ref_name,
+        'ref_link': self.ref_link,
+        'ref_type': self.ref_type,
+        'ref_description': self.ref_description,
+        'ref_status': self.ref_status
+        }
+        # fragment = Fragment()
+        # fragment.add_content(loader.render_template('referencexblock.html', context))
+        # return fragment   
+
+        fragment = Fragment(loader.render_template("static/html/referencexblock.html", context))
+        fragment.add_css(self.resource_string("static/css/referencexblock.css"))
+        fragment.add_javascript(
+        self.resource_string("static/js/src/referencexblock.js"))
+        fragment.initialize_js("ReferenceXBlock")
+        return fragment
+
+    @XBlock.json_handler
+    def reference_data(self, data, suffix=''):
+        """
+        An example handler, gets data from template.
+        """
+        # Just to show data coming in...
+ 
+        self.ref_name = data['ref_name']
+        self.ref_link = data['ref_link']
+        self.ref_type = data['ref_type']
+        self.ref_description = data['ref_description']
+        self.ref_status = data['ref_status']
+
+
+        return {"result": "success"}
+        
+        # return {"count": self.count}
+
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("ReferenceXBlock",
+             """<referencexblock/>
+             """),
+            ("Multiple ReferenceXBlock",
+             """<vertical_demo>
+                <referencexblock/>
+                <referencexblock/>
+                <referencexblock/>
+                </vertical_demo>
+             """),
+        ]
